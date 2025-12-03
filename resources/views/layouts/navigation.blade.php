c23c67fa363575d000be72dff975a222d5265670
salmankavanur <mpsalman2011@gmail.com>
2025-12-03 12:40:29 +0530

    added padding ml-3 for the unread chat badge in both the admin and client sidebar menus

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -175,7 +175,7 @@
                     <div class="flex justify-between items-center w-full">
                         <span>{{ __('Chat') }}</span>
                         @if($unreadChatCount > 0)
-                            <span class="bg-red-500 text-white text-xs font-bold px-2 py-0.5 rounded-full">{{ $unreadChatCount }}</span>
+                            <span class="bg-red-500 text-white text-xs font-bold px-2 py-0.5 rounded-full ml-3">{{ $unreadChatCount }}</span>
                         @endif
                     </div>
                 </x-sidebar-link>
@@ -239,7 +239,7 @@
                     <div class="flex justify-between items-center w-full">
                         <span>{{ __('Chat') }}</span>
                         @if($unreadChatCount > 0)
-                            <span class="bg-red-500 text-white text-xs font-bold px-2 py-0.5 rounded-full">{{ $unreadChatCount }}</span>
+                            <span class="bg-red-500 text-white text-xs font-bold px-2 py-0.5 rounded-full ml-3">{{ $unreadChatCount }}</span>
                         @endif
                     </div>
                 </x-sidebar-link>
